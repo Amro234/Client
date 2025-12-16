1261a10aa03e2febd3dafb7cfca13c6d6fa85cd8
EslamElnady0 <eslamelnady254@gmail.com>
2025-12-16 04:25:37 +0200

    finish basic audio settings

--- Client/src/main/java/com/mycompany/client/App.java
+++ Client/src/main/java/com/mycompany/client/App.java
@@ -17,7 +17,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("settings"), 1280, 720);
+        scene.getStylesheets().add(
+        getClass().getResource("/styles/customStyles.css").toExternalForm()
+    );
+
         stage.setScene(scene);
         stage.show();
     }
