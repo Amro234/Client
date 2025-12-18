318b730d6a4dc096bb42062c227880f7469bcf82
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-12-18 19:37:36 +0200

    add logic to logout btn leave / stay in app according to user choice

--- Client/src/main/java/com/mycompany/client/GameLobbyController.java
+++ Client/src/main/java/com/mycompany/client/GameLobbyController.java
@@ -5,8 +5,16 @@
 package com.mycompany.client;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 
 /**
  * FXML Controller class
@@ -14,6 +22,9 @@ import javafx.fxml.Initializable;
  * @author Mohamed_Ali
  */
 public class GameLobbyController implements Initializable {
+
+    @FXML
+    private Button logOutBtn;
 
     /**
      * Initializes the controller class.
@@ -23,4 +34,38 @@ public class GameLobbyController implements Initializable {
         // TODO
     }    
     
+     
+
+    @FXML
+    private void onLogOutPressed(ActionEvent event) {
+         showConfirmationDialog("Info", "Are you sure you want to leave the app?");
+    }
+    
+    
+    private void showConfirmationDialog(String title, String message) {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle(title);
+    alert.setHeaderText(message);
+    alert.setContentText("Choose your option:");
+
+    ButtonType okButton = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+    ButtonType cancelButton = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+    alert.getButtonTypes().setAll(okButton, cancelButton);
+
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.isPresent() && result.get() == okButton) {
+        showGoodbyeMessage();
+}
+}
+
+private void showGoodbyeMessage() {
+    Alert goodbyeAlert = new Alert(Alert.AlertType.INFORMATION);
+    goodbyeAlert.setTitle("Goodbye");
+    goodbyeAlert.setHeaderText("Thank you for using the app!");
+    goodbyeAlert.setContentText("We hope to see you again soon.");
+    goodbyeAlert.showAndWait();
+
+  
+    Platform.exit();
+}
 }
