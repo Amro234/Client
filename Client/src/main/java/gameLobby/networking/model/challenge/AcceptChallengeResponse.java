020e7c8d8cdbe0336dafbda7776491086bc14353
EslamElnady0 <eslamelnady254@gmail.com>
2025-12-21 07:08:54 +0200

    added accept challenge response

---
+++ Client/src/main/java/gameLobby/networking/model/challenge/AcceptChallengeResponse.java
@@ -0,0 +1,25 @@
+package gameLobby.networking.model.challenge;
+
+public class AcceptChallengeResponse {
+    private final boolean success;
+    private final String message;
+    private final Challenge opponent;
+
+    public AcceptChallengeResponse(boolean success, String message, Challenge opponent) {
+        this.success = success;
+        this.message = message;
+        this.opponent = opponent;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Challenge getOpponent() {
+        return opponent;
+    }
+}
